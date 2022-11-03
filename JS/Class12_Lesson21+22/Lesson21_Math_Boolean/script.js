1c01d11a912647e544f215ebd1ebfa25d1502c23
itamar9ic <itamar9ic@gmail.com>
2022-11-04 01:08:59 +0200

    Fullstack_Course-exercises

---
+++ JS/Class12_Lesson21+22/Lesson21_Math_Boolean/script.js
@@ -0,0 +1,32 @@
+
+// 1
+let q = (Math.PI)
+console.log(q);
+
+// 2
+let a = (Math.PI);
+console.log(Math.round(a));
+
+// 3
+let z = 15 != 10;
+console.log(z);
+
+// 4
+let w = 50 == 50;
+console.log(w);
+
+// 5
+let s = -18 < 13;
+console.log(s);
+
+// 6
+function jelly(x){
+
+    if (x != 30) {
+        return false;
+    }
+    
+
+};
+console.log(jelly());
+
---
+++ JS/Class12_Lesson21+22/Lesson22_Random_Loops/script.js
@@ -0,0 +1,17 @@
+
+// 1
+function getRndIntgr(min,max) {
+    return 
+}
+
+let x = 10;
+let y = 15;
+
+let z = Math.random(x, y) 
+
+console.log(z);
+
+// 2
+somePrhase = "Hello World"
+
+console.log(somePrhase);
