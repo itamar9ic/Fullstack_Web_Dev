a440bf653ab7798c70d9447a648901672d33430e
itamar9ic <itamar9ic@gmail.com>
2022-11-04 02:52:02 +0200

    Lesson22_Random_Loops

--- JS/Class12_Lesson21+22/Lesson22_Random_Loops/script.js
+++ JS/Class12_Lesson21+22/Lesson22_Random_Loops/script.js
@@ -1,17 +1,32 @@
-
+// const multiplier = 10;
+// for (let i = 0; i < 100; i++) {
+//     let result = multiplier * i+10; 
+//     console.log(result);
+// }
+    
 // 1
-function getRndIntgr(min,max) {
-    return 
+
+// option 1
+// let x  = 10;
+// let y  = 15;
+// function belly(max,min) {
+//     let rand = Math.floor(Math.random() *(max - min + 1)) + min;
+//     return console.log(rand);
+//     }
+//     belly(x,y);
+
+// option 2
+function getRndInteger(min,max) {
+    return Math.floor(Math.random() *(max - min +1)) + min;
 }
+console.log(getRndInteger(10,15));
 
-let x = 10;
-let y = 15;
-
-let z = Math.random(x, y) 
-
-console.log(z);
 
 // 2
-somePrhase = "Hello World"
+let random = "presentation";
+for (let i = 0; i < random.length; i++) {
 
-console.log(somePrhase);
+    console.log(random[i]);
+
+}
+
