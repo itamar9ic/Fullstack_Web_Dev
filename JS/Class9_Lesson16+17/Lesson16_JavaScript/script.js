19273ff451598b40b69710aef026e9d0eff35c7a
itamar9ic <itamar9ic@gmail.com>
2022-11-05 20:46:29 +0200

    111111111

--- JS/Class9_Lesson16+17/Lesson16_JavaScript/script.js
+++ JS/Class9_Lesson16+17/Lesson16_JavaScript/script.js
@@ -1,3 +1,68 @@
+// let human = "John Doe", gender = "Male", age = 25, height = 5.8, weight = 65, married = false, profession = "Engineer";
+// console.log("Full Name: " + human);
+
+
+// const parseon ={
+//     name: "John Doe",
+//     gender : "Male"
+// };
+
+// console.log(typeof(parseon));
+// console.log(typeof(human));    
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// };
+
+
+// javascript arithmetic operators:
+/*
+    + addition
+    - subtraction
+    * multiplication
+    / division
+    % modulus
+    ** exponentiation
+    ++ increment
+    -- decrement
+*/
+
+// javascript assignment operators:
+/*
+    = assignment
+    += addition assignment
+    -= subtraction assignment
+    *= multiplication assignment
+    /= division assignment
+    %= modulus assignment
+    **= exponentiation assignment
+*/
+
+// אופרטור ההקצאה (=) מקצה ערך למשתנה.
+// אופרטור החיבור (+) מוסיף מספרים.
+// אופרטור הכפל (*) מכפיל מספרים.
+// אופרטור החיסור (-) מפחית מספרים.
+// אופרטור החזרה (**) בחזקת.
+// אפורטור החילוק (/) יחלק מספרים.
+// אופרטור "מודולו" (%) שארית החלוקה.
+// אופרטור הקידום (++) יקדם את ערך המשתנה באחד.
+// אופרטור ההפחתה (--) מוריד מפחית אחד ומחזיר ערך.
+// אופרטור הקצאת החיבור (=+) מוסיף את הערך של האופרנד הימני למשתנה ומקצה את התוצאה למשתנה.
+// אופרטור הקצאת החיסור (-=) מוריד את הערך הימני ממשתנה ומקצה את התוצאה למשתנה.
+// אופרטור השוויון (==) בודק אם שני האופרנדים שלו שווים, ומחזיר תוצאה בוליאנית. בניגוד לאופרטור השוויון הקפדני, הוא מנסה להמיר ולהשוות אופרנדים מסוגים שונים.
+// אופרטור השוויון הקפדני (===) בודק אם הערכים שווים בערך ובסוג שלהם.
+// אופרטור הקטן מ (>)
+// אופרטור הגדול מ (<)
+// אופרטור הקטן שווה (=>) כלומר הערך קטן מ או שווה ל
+// אופרטור הגדול שווה (=>) כלומר הערך גדול מ או שווה ל
+
+// x = 8;
+// y = 26;
+// z = 3;
+// v = 0; 
+// b = 5;
+// total = x + y * z * v + b; 
+// console.log(total);
 
 // node script.js
 
