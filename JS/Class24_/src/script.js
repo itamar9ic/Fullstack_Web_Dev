5c23ae272a7835ceebdf94e958809fde80322050
itamar9ic <itamar9ic@gmail.com>
2022-12-02 00:21:07 +0200

    2/12/22

--- JS/Class24_/src/script.js
+++ JS/Class24_/src/script.js
@@ -1,21 +1,22 @@
-function onsigninbutton() {
-    
+function login() {
+   let json = fetch("./data_base.json")
+   .then
     let sUserName = document.getElementById("user_name").value;
     let sPassword = document.getElementById("password").value;
 
-    if (sUserName === 'username' && sPassword === 'password' || sUserName === 'itamar' && sPassword === '1234') {
+    if (sUserName + sPassword === data_base.json) {
      alert("wellcome username");
     } else {
      alert("login failed"); 
     }
-
+console.log(json);
  }
 
- function fetchData(){
-    let json = fetch("./data_base.json")
-    .then(response => response.json())
-    .then(data => console.log(data));
+//  function fetchData(){
+//     let json = fetch("./data_base.json")
+//     .then(response => response.json())
+//     .then(data => console.log(data));
     
-    console.log(json);
-    }
+//     console.log(json);
+//     }
     
