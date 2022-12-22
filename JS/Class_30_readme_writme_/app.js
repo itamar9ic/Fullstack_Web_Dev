f358ef36ddb1de6f7edb357125f4c6aa19440219
itamar9ic <itamar9ic@gmail.com>
2022-12-22 21:00:27 +0200

    22/12/22

--- JS/Class_30_readme_writme_/app.js
+++ JS/Class_30_readme_writme_/app.js
@@ -3,7 +3,7 @@
 
 // fs module is a core module, so we don't need to install it
 
-// import { readFile, writeFile, appendFile } from 'fs';
+// import { readFile, writeFile, appendFile, readFileSync} from 'fs';
 
 // readFile is an asynchronous function
 // readFile takes 3 arguments: file name, encoding, callback function
@@ -47,27 +47,44 @@
 
 // // Q.1: split the data from readme in to an array of substrings (words). and print it!
 
+// let Data_base
+// async function f(){
+//   await new Promise((resolve, reject) => {
+//     readFile("ex1.txt", "utf8", (error, data) => {
+//       if(error){
+//         console.log("cannot find file");
+//       }
+//       resolve(data)
+//     })
+//   }).then(data => {Data_base = data.split(" ")});
+// }
+
+// await f()
+// console.log(Data_base)
+
 // node app.js
 
 // const { rejects } = require('assert');
 const fs = require('fs');
 // const { resolve } = require('path');
 
-let Data_base
-async function splitData() {
-    await new Promise((resolve, rejects) => {
-        fs.readFile('readme.txt', 'utf8', (error, data) => {
-            if (error) {
-                console.log("Error");
-            } resolve(data)
-        })
+// let Data_base
+// async function splitData() {
+//     await new Promise((resolve, rejects) => {
+//         fs.readFile('readme.txt', 'utf8', (error, data) => {
+//             if (error) {
+//                 console.log("Error");
+//             } resolve(data)
+//         })
+//     }).then(data => {
+//         Data_base = data.split("\n")
+//     });
+// }
 
-    }).then(data => {
-        Data_base = data.split("\n")
-    });
-}
+// await splitData();
+// console.log(Data_base);
 
-await splitData();
-console.log(Data_base);
+console.log(fs.readFileSync("ex.txt", "utf8").split(" "));
+
 
 // Q.2: find the word that appeard the maximum amount of time in the text.
