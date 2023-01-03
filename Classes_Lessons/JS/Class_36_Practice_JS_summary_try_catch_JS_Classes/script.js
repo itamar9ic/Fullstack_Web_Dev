7474ea246d1820f60942bc1b8e168ee36e5b100b
itamar9ic <itamar9ic@gmail.com>
2023-01-03 22:06:56 +0200

    3/1/23

---
+++ Classes_Lessons/JS/Class_36_Practice_JS_summary_try_catch_JS_Classes/script.js
@@ -0,0 +1,25 @@
+// Exercise 1
+
+// 1. Write a function that takes a string as input and returns the string with all vowels removed. 
+// For example, the string "javascript" should return "jvscrpt".
+
+let vowels = [a,e,i,o,u]
+
+tr
+// 2. Write a function that takes an array of numbers as input and returns an array of all the even numbers. 
+// For example, the array [1, 2, 3, 4, 5] should return [2, 4].
+
+// = %2
+
+// 3. Write a function that takes an array of strings as input and returns an array of all the strings that have more than 5 characters. 
+// For example, the array ["apple", "banana", "grape", "pear"] should return ["banana", "grape"].
+
+
+
+// 4. Write a function that takes an array of objects as input and returns an array of all the objects that have a certain property. 
+// For example, given the array [{name: "Alice", age: 25}, {name: "Bob", age: 30}, {name: "Eve", age: 35}], the function should return an array of all the objects that have an age property greater than 30.
+
+
+
+// 5. Write a function that takes a string and a number as input and returns a new string with the original string repeated the number of times specified by the number. 
+// For example, the string "hello" and the number 3 should return "hellohellohello".
